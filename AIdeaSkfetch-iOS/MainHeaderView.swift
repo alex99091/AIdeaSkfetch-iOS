69796bc57c23fbfe06da8d35de93822c90418a66
alex99091 <bkk99091@gmail.com>
2023-02-07 22:55:11 +0900

    Create basic set-up file as UIKit version

---
+++ AIdeaSkfetch-iOS/MainHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  MainHeaderView.swift
+//  AIdeaSkfetch-iOS
+//
+//  Created by BOONGKI KWAK on 2023/02/07.
+//
+
+import UIKit
+
+class MainHeaderView: UICollectionReusableView {
+    static let reuseIdentifier = String(describing: MainHeaderView.self)
+}
+
