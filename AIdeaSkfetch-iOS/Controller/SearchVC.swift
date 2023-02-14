2c9756fa2465e50ab42607d045eebb419ba1e366
alex99091 <bkk99091@gmail.com>
2023-02-14 23:27:59 +0900

    SearchVC created

---
+++ AIdeaSkfetch-iOS/Controller/SearchVC.swift
@@ -0,0 +1,24 @@
+//
+//  SearchVC.swift
+//  AIdeaSkfetch-iOS
+//
+//  Created by BOONGKI KWAK on 2023/02/14.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+ 
+    // MARK: - Outlet
+    
+    // MARK: - Property
+    
+    // MARK: - VC LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Method
+    
+    
+}
